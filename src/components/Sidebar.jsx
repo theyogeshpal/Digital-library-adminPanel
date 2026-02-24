2107d7e2574079aff01af15c71cbff0dc2b14113
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-02-24 20:11:20 +0530

    redirect file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,11 +32,8 @@ const Sidebar = ({ isOpen, onClose }) => {
         </button>
 
         <div className="p-6 border-b border-slate-700/50">
-          <div className="flex items-center gap-3">
-            <div className="p-2 bg-gradient-to-br from-blue-500 to-purple-600 rounded-xl">
-              <Sparkles className="w-6 h-6" />
-            </div>
-            <h2 className="text-xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">Admin Panel</h2>
+          <div>
+            <img src='logo/horizontal-logo.png' className=" invert font-bold scale-150" />
           </div>
         </div>
         <nav className="mt-8 px-3">
@@ -64,7 +61,7 @@ const Sidebar = ({ isOpen, onClose }) => {
           ))}
         </nav>
         <div className="absolute bottom-6 w-full px-6">
-          <button className="flex items-center gap-4 w-full px-4 py-3.5 hover:bg-red-600/20 rounded-xl transition-all duration-300 hover:translate-x-1 border border-slate-700/50 group">
+          <button className="flex items-center justify-center gap-4 w-full px-4 py-3.5 bg-red-500 rounded-xl  border border-slate-700/50 group">
             <LogOut className="w-5 h-5 group-hover:scale-110 transition-transform" />
             <span className="font-medium">Logout</span>
           </button>
